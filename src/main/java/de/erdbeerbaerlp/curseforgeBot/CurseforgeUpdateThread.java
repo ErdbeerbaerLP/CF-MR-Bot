a6efaa6d8dcbdb179d692410784719406d6ab65b
ROMVoid <rom.void95@gmail.com>
2020-02-15 04:12:52 -0600

    add channel specific roles and clean up run()

--- src/main/java/de/erdbeerbaerlp/curseforgeBot/CurseforgeUpdateThread.java
+++ src/main/java/de/erdbeerbaerlp/curseforgeBot/CurseforgeUpdateThread.java
@@ -12,13 +12,21 @@ import net.dv8tion.jda.api.entities.TextChannel;
 public class CurseforgeUpdateThread extends Thread
 {
     private final CurseProject proj;
-    private final String channelID;
-    
+    private String channelID;
+    private String roleID;
     
     CurseforgeUpdateThread(String id) throws CurseException {
         if (id.contains(";;")) {
-            channelID = id.split(";;")[1];
+            String[] ids = id.split(";;");
+            channelID = ids[1];
+            if (ids.length == 3) {
+        	System.out.println(ids.length);
+        	roleID = ids[2];
+            }
         }
+        else
+    	roleID = Main.cfg.mentionRole;
+        channelID = Main.cfg.DefaultChannel;
         else channelID = Main.cfg.DefaultChannel;
         final Optional<CurseProject> project = CurseAPI.project(Integer.parseInt(id.split(";;")[0]));
         if (!project.isPresent()) throw new CurseException("Project not found");
@@ -33,25 +41,11 @@ public class CurseforgeUpdateThread extends Thread
                 System.out.println("<" + proj.name() + "> Cached: " + Main.cache.get(proj.name()) + " Newest:" + proj.files().first().id());
                 if (Main.cfg.isNewFile(proj.name(), proj.files().first().id())) {
                     TextChannel channel = Main.jda.getTextChannelById(channelID);
-                    String roleid = Main.cfg.mentionRole;
-                    if (Main.cfg.updateFileLink.equals("nolink")) {
-                	if (!roleid.equals("000000000")) {
-                    	channel.sendMessage(EmbedMessage.getRoleAsMention(roleid, channel)).queue();
-			}
-                        EmbedMessage.messageWithoutLink(proj, proj.files().first(), channel);
-		    }
-                    if (Main.cfg.updateFileLink.equals("curse")) {
-                	if (!roleid.equals("000000000")) {
-                        	channel.sendMessage(EmbedMessage.getRoleAsMention(roleid, channel)).queue();
-    			}
-                        EmbedMessage.messageWithCurseLink(proj, proj.files().first(), channel);
-		    }
-                    if (Main.cfg.updateFileLink.equals("direct")) {
-                	if (!roleid.equals("000000000")) {
-                        	channel.sendMessage(EmbedMessage.getRoleAsMention(roleid, channel)).queue();
-    			}
-                        EmbedMessage.messageWithDirectLink(proj, proj.files().first(), channel);
-		    }
+                    Role role = channel.getGuild().getRoleById(roleID);
+                    if(!(role == null)) {
+                	EmbedMessage.sendPingableUpdateNotification(role, channel, proj);
+                    } 
+                    else EmbedMessage.sendUpdateNotification(channel, proj);
                     Main.cache.put(proj.name(), proj.files().first().id());
                     Main.cacheChanged = true;
                 }
